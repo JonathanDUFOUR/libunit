5e600c5dd2d9511a3abfdd54d805e0965dab9984
jodufour <jodufour@student.42.fr>
2022-01-09 16:07:56 +0100

    set unit_run utils to static functions [OK]

--- framework/include/unit.h
+++ framework/include/unit.h
@@ -6,16 +6,20 @@
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/08 05:17:30 by jodufour          #+#    #+#             */
-/*   Updated: 2022/01/09 15:17:10 by                  ###   ########.fr       */
+/*   Updated: 2022/01/09 15:50:48 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef UNIT_H
 # define UNIT_H
 
+# ifndef TIMEOUT_VALUE
+#  define TIMEOUT_VALUE 1
+# endif
+
 # include <stddef.h>
 
-int		redirect_init(void);
-int		redirect_end(void);
+int	redirect_init(void);
+int	redirect_end(void);
 
 #endif
--- framework/srcs/unit/unit_run.c
+++ framework/srcs/unit/unit_run.c
@@ -6,7 +6,7 @@
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/08 07:20:05 by jodufour          #+#    #+#             */
-/*   Updated: 2022/01/09 15:10:08 by                  ###   ########.fr       */
+/*   Updated: 2022/01/09 15:48:08 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,36 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include "unit.h"
 #include "t_unit.h"
 #include "internal_functions.h"
 
-int		translate_signal(int raw_status);
-void	execute_tests(t_unit *const node);
-void	execute_watchdog(void);
+static void	__execute_watchdog(void)
+{
+	sleep(TIMEOUT_VALUE);
+	exit(-2);
+}
+
+static void	__execute_tests(t_unit *const node)
+{
+	t_fct	call;
+	int		status;
+
+	call = node->call;
+	output_name(node->funcname, node->testname);
+	unit_clear(node);
+	status = call();
+	exit(status);
+}
+
+static int	__translate_signal(int raw_status)
+{
+	if (WIFEXITED(raw_status))
+		return ((char)WEXITSTATUS(raw_status));
+	else if (WIFSIGNALED(raw_status))
+		return (WTERMSIG(raw_status));
+	return (raw_status);
+}
 
 /*
 	Run the function contained in the given unit `node` in a child process
@@ -33,44 +57,15 @@ int	unit_run(t_unit *const node)
 	int		status;
 
 	worker = fork();
-	watchdog = -1;
 	if (worker == -1 || write(STDOUT_FILENO, "", 0) == -1)
 		return (EXIT_FAILURE);
-	if (! worker)
-		execute_tests(node);
-	else
-		watchdog = fork();
-	if (! watchdog)
-		execute_watchdog();
-	if (wait(&status) == -1)
+	if (!worker)
+		__execute_tests(node);
+	watchdog = fork();
+	if (!watchdog)
+		__execute_watchdog();
+	if (wait(&status) == -1 || wait(NULL) == -1)
 		return (EXIT_FAILURE);
-	status = translate_signal(status);
+	status = __translate_signal(status);
 	return (output_status(status));
-}
-
-void	execute_watchdog(void)
-{
-	sleep(1);
-	exit(-2);
-}
-
-void	execute_tests(t_unit *const node)
-{
-	t_fct	call;
-	int		status;
-
-	call = node->call;
-	output_name(node->funcname, node->testname);
-	unit_clear(node);
-	status = call();
-	exit(status);
-}
-
-int	translate_signal(int raw_status)
-{
-	if (WIFEXITED(raw_status))
-		raw_status = (char)WEXITSTATUS(raw_status);
-	else if (WIFSIGNALED(raw_status))
-		raw_status = WTERMSIG(raw_status);
-	return (raw_status);
 }
