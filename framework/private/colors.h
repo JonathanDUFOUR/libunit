1c722787ebe0303ce94e8085f05d8963a58a2638
jodufour <jodufour@student.42.fr>
2022-10-29 07:10:46 +0000

    Update colors defines [OK]

--- framework/private/colors.h
+++ framework/private/colors.h
@@ -6,21 +6,34 @@
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/08 08:38:04 by jodufour          #+#    #+#             */
-/*   Updated: 2022/01/08 09:19:39 by jodufour         ###   ########.fr       */
+/*   Updated: 2022/10/29 07:09:17 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef COLORS_H
 # define COLORS_H
 
-# define BLACK		"\e[30m"
-# define RED		"\e[31m"
-# define GREEN		"\e[32m"
-# define YELLOW		"\e[33m"
-# define BLUE		"\e[34m"
-# define MAGENTA	"\e[35m"
-# define CYAN		"\e[36m"
-# define WHITE		"\e[37m"
-# define RESET		"\e[0m"
+# define BLACK_FG	"\033[38;2;0;0;0m"
+# define RED_FG		"\033[38;2;255;0;0m"
+# define GREEN_FG	"\033[38;2;0;255;0m"
+# define BLUE_FG	"\033[38;2;0;0;255m"
+# define YELLOW_FG	"\033[38;2;255;255;0m"
+# define MAGENTA_FG	"\033[38;2;255;0;255m"
+# define CYAN_FG	"\033[38;2;0;255;255m"
+# define WHITE_FG	"\033[38;2;255;255;255m"
+
+# define BLACK_BG	"\033[48;2;0;0;0m"
+# define RED_BG		"\033[48;2;255;0;0m"
+# define GREEN_BG	"\033[48;2;0;255;0m"
+# define BLUE_BG	"\033[48;2;0;0;255m"
+# define YELLOW_BG	"\033[48;2;255;255;0m"
+# define MAGENTA_BG	"\033[48;2;255;0;255m"
+# define CYAN_BG	"\033[48;2;0;255;255m"
+# define WHITE_BG	"\033[48;2;255;255;255m"
+
+# define BOLD		"\033[1m"
+# define BLINK		"\033[5m"
+
+# define RESET		"\033[0m"
 
 #endif
--- framework/private/lookup_result.h
+++ framework/private/lookup_result.h
@@ -6,7 +6,7 @@
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/08 07:59:42 by jodufour          #+#    #+#             */
-/*   Updated: 2022/01/09 22:37:23 by jodufour         ###   ########.fr       */
+/*   Updated: 2022/10/06 18:41:25 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,15 +25,15 @@ struct s_result
 };
 
 static t_result const	g_result[] = {
-{0, GREEN "OK" RESET},
-{-1, RED "KO" RESET},
-{-2, RED "TIMEOUT" RESET},
-{SIGSEGV, RED "SIGSEGV" RESET},
-{SIGBUS, RED "SIGBUS" RESET},
-{SIGABRT, RED "SIGABRT" RESET},
-{SIGFPE, RED "SIGFPE" RESET},
-{SIGILL, RED "SIGILL" RESET},
-{SIGPIPE, RED "SIGPIPE" RESET},
+{0, GREEN_FG "OK" RESET},
+{-1, RED_FG "KO" RESET},
+{-2, RED_FG "TIMEOUT" RESET},
+{SIGSEGV, RED_FG "SIGSEGV" RESET},
+{SIGBUS, RED_FG "SIGBUS" RESET},
+{SIGABRT, RED_FG "SIGABRT" RESET},
+{SIGFPE, RED_FG "SIGFPE" RESET},
+{SIGILL, RED_FG "SIGILL" RESET},
+{SIGPIPE, RED_FG "SIGPIPE" RESET},
 {0}
 };
 
